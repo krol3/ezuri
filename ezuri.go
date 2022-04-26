f465a459a3f1e0f3ef755c686f22ad063f4a58d3
carolina valencia <krol3@users.noreply.github.com>
2022-04-25 23:08:03 -0500

    feat: go mod version

--- ezuri.go
+++ ezuri.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"os/exec"
 	"text/template"
+	"time"
 )
 
 const (
@@ -74,4 +78,71 @@ func main() {
 	w.Write(encryptedBytes)
 	w.Flush()
 	fmt.Println("[!] All done!")
+}
+
+// ## UTILS
+
+func check(e error) {
+	// Reading files requires checking most calls for errors.
+	// This helper will streamline our error checks below.
+	if e != nil {
+		panic(e)
+	}
+}
+
+func userInput(stubCfg *stubConfig) (string, string) {
+	var srcFilePath string
+	fmt.Print("[?] Path of file to be encrypted: ")
+	fmt.Scanln(&srcFilePath)
+
+	var dstFilePath string
+	fmt.Print("[?] Path of output (encrypted) file: ")
+	fmt.Scanln(&dstFilePath)
+
+	fmt.Print("[?] Name of the target process: ")
+	fmt.Scanln(&stubCfg.ProcName)
+
+	fmt.Print("[?] Encryption key (32 bits - random if empty): ")
+	fmt.Scanln(&stubCfg.EncryptionIV)
+	fmt.Print("[?] Encryption IV (16 bits - random if empty): ")
+	fmt.Scanln(&stubCfg.EncryptionIV)
+	if stubCfg.EncryptionKey == "" {
+		stubCfg.EncryptionKey = randKey(modeKey)
+		stubCfg.EncryptionIV = randKey(modeIV)
+	}
+	fmt.Println()
+	fmt.Printf("[!] Random encryption key (used in stub): %s\n", stubCfg.EncryptionKey)
+	fmt.Printf("[!] Random encryption IV (used in stub): %s\n", stubCfg.EncryptionIV)
+	return srcFilePath, dstFilePath
+}
+
+func randKey(mode int) string {
+	var keySize int
+
+	if mode == modeIV {
+		keySize = 16
+	} else if mode == modeKey {
+		keySize = 32
+	}
+
+	key := make([]byte, keySize)
+	for i := range key {
+		key[i] = allowedChars[rand.Intn(len(allowedChars))]
+	}
+	return string(key)
+}
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+// ## aes.go
+func aesEnc(srcBytes []byte, key string, iv string) []byte {
+	block, err := aes.NewCipher([]byte(key))
+	check(err)
+
+	encrypter := cipher.NewCFBEncrypter(block, []byte(iv))
+	encrypted := make([]byte, len(srcBytes))
+	encrypter.XORKeyStream(encrypted, srcBytes)
+	return encrypted
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/krol3/ezuri
+
+go 1.18
